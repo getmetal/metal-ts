1364cb55833ea72c2f82ae6631e6356ea0f576fc
James O'Dwyer <james@getmetal.io>
2023-02-05 20:29:44 -0800

    turn the sdk into a class.

--- README.md
+++ README.md
@@ -14,6 +14,9 @@ Then in your app you can use it to index.
 ```javascript
 import Metal from '@getmetal/metal-sdk'
 
-await Metal.index('Data to index! Heavy metal is the law', appId)
+
+const metal = new Metal('app-id');
+
+await metal.index('Data to index! Heavy metal is the law');
 
 ```
--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,29 @@ import axios from 'axios';
 
 const API_URL: string = 'https://api.getmetal.io';
 
-export function index(input: string, appId: string): Promise<object> {
-  return axios.post(
-    `${API_URL}/v1/index`,
-    { input, app: appId },
-    { headers: { 'Content-Type': 'application/json' } }
-  );
+
+class MetalSDK {
+  appId: string;
+
+  constructor(appId: string) {
+    this.appId = appId;
+  }
+
+  index(input: string) {
+    return axios.post(
+      `${API_URL}/v1/index`,
+      { input, app: this.appId },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+
+  search(input: String) {
+    return axios.post(
+      `${API_URL}/v1/search`,
+      { input, app: this.appId },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  }
 }
 
-export function search(input: string, appId: string): Promise<object> {
-  return axios.post(
-    `${API_URL}/v1/search`,
-    { input, app: appId },
-    { headers: { 'Content-Type': 'application/json' } }
-  );
-}
-
-export default { index, search };
+export default MetalSDK;
