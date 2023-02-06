0f44e379d139a38a3987a7c99fe7b670ffb79d7b
softboyjimbo <100361543+softboyjimbo@users.noreply.github.com>
2023-02-05 20:53:32 -0800

    Classify the SDK (#1)
    * turn the sdk into a class.
    
    * add promise return.
    
    * add app id.
    
    * ref the correct app.

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,51 @@ import axios from 'axios';
 
 const API_URL: string = 'https://api.getmetal.io';
 
-export function index(input: string, appId: string): Promise<object> {
-  return axios.post(
-    `${API_URL}/v1/index`,
-    { input, app: appId },
-    { headers: { 'Content-Type': 'application/json' } }
-  );
+
+class MetalSDK {
+  apiKey: string;
+  clientId: string;
+  appId?: string;
+
+  constructor(apiKey: string, clientId: string, appId?: string) {
+    this.apiKey = apiKey;
+    this.clientId = clientId;
+    this.appId = appId;
+  }
+  
+
+  index(input: string, appId?: string): Promise<object> {
+    const app = appId || this.appId;
+    if (!app) {
+      throw new Error('appId required');
+    }
+
+    return axios.post(
+      `${API_URL}/v1/index`,
+      { input, app },
+      { headers: {
+        'Content-Type': 'application/json',
+        'x-metal-api-key': this.apiKey,
+        'x-metal-client-id': this.clientId,
+      } }
+    );
+  }
+
+  search(input: string, appId?: string): Promise<object> {
+    const app = appId || this.appId;
+    if (!app) {
+      throw new Error('appId required');
+    }
+    return axios.post(
+      `${API_URL}/v1/search`,
+      { input, app },
+      { headers: {
+        'Content-Type': 'application/json',
+        'x-metal-api-key': this.apiKey,
+        'x-metal-client-id': this.clientId,
+      } }
+    );
+  }
 }
 
-export function search(input: string, appId: string): Promise<object> {
-  return axios.post(
-    `${API_URL}/v1/search`,
-    { input, app: appId },
-    { headers: { 'Content-Type': 'application/json' } }
-  );
-}
-
-export default { index, search };
+export default MetalSDK;
