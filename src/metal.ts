2c158fa720211da243cd84c8fdb6f0020da8a5a5
Sergio Prada <chechopradaster@gmail.com>
2023-03-31 12:23:46 -0700

    add delete_one

--- src/metal.ts
+++ src/metal.ts
@@ -137,6 +137,22 @@ class MetalSDK {
 
     return data
   }
+
+  async deleteOne(id: string): Promise<object> {
+    if (!id) {
+      throw new Error('id required')
+    }
+
+    const { data } = await axios.delete(`${API_URL}/v1/documents/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-metal-api-key': this.apiKey,
+        'x-metal-client-id': this.clientId,
+      },
+    })
+
+    return data
+  }
 }
 
 export default MetalSDK
