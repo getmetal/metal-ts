6dfe7392dd9b239b04850a6bd025c3ed2c26e092
James O'Dwyer <james@getmetal.io>
2023-06-12 09:32:32 -0400

    update imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import Metal from './metal'
 import Motorhead from './motorhead'
 
-export {
-  Metal,
-  Motorhead,
-  Metal as Retriever,
-  Motorhead as Memory,
-}
+export { Metal, Motorhead, Metal as Retriever, Motorhead as Memory }
